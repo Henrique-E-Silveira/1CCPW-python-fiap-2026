52060130102fb60f41d5c7f3d7d8eced98690f65
labsfiap <henriqueetec12345@gmail.com>
2026-03-16 22:43:41 -0300

    fazendo atividade

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,16 @@
+
+
+nome = input("Digite seu nome: ")
+
+print(nome)
+
+dia = input("Agora digite o dia em que você nasceu: ")
+mes = input("Agora digite o mês em que você nasceu: ")
+ano = input("Agora digite o ano em que você nasceu: ")
+print(f'Você nasceu em: {int(dia)}/{int(mes)}/{int(ano)} ')
+
+x = input("Digite o primeiro número a ser somado: ")
+y = input("Digite o segundo número a ser somado: ")
+x = int(x) + int(y)
+print(f'A soma entre eles é: {int(x)} ')
+
